1d256594ad5bf786f38615310f71adcd1ea73ad9
unknown <abhinashm606@gmail.com>
2025-10-10 14:27:20 +0530

    first commit

--- One.java
+++ One.java
@@ -188,17 +188,14 @@ Scanner obj=new Scanner(System.in);
 option=obj.nextInt();
 
 
-switch(option)
+ String result = switch(option)
 {
-    case 1:System.out.println("case1");
-    break;
-
-    case 2:System.out.println("case2");
-     break;
-    case 3:System.out.println("case3");
-     break;
-    default:System.out.println("invailid");
-}
+    case 1-> "case1";
+    case 2-> "case2";
+    case 3-> "case3";
+    default-> "invailid";
+};
+System.out.println(result);
 }
 }
 
